343258a38960b781bd3ae98b68c748c1a8be8c73
SaddamHossain07 <contact.saddam3@gmail.com>
2023-11-08 21:41:47 +0600

    finalizing project

--- index.js
+++ index.js
@@ -109,13 +109,13 @@ async function run() {
             res.send(result)
         })
 
-        app.post('/books', async (req, res) => {
+        app.post('/books', verifyToken, async (req, res) => {
             const book = req.body
             const result = await booksCollection.insertOne(book)
             res.send(result)
         })
 
-        app.put('/books/:id', async (req, res) => {
+        app.put('/books/:id', verifyToken, async (req, res) => {
             const id = req.params.id
             const query = { _id: new ObjectId(id) }
             const options = { upsert: true }
